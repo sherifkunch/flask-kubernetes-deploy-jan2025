0b53c2566f821753eab12125e1866480d0beda35
Sherif Kunch <sherifkunch@gmail.com>
2025-01-21 21:54:30 +0200

    testing my pipeline

--- code/app.py
+++ code/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello World from Flask2</h2>'
+    return '<h1>Hello World from Flask. This is my change to demonstrate that my demo is working hopefully!</h2>'
 
 if __name__ == "__main__":
     app.run(debug=True)
